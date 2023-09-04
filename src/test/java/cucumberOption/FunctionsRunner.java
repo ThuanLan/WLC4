4e8da9410ebc572d1c80c03cd58d8e092f10e5cf
ThuanLan <thuanlanmd@gmail.com>
2023-09-04 15:02:56 +0700

    update

--- src/test/java/cucumberOption/FunctionsRunner.java
+++ src/test/java/cucumberOption/FunctionsRunner.java
@@ -26,7 +26,7 @@ import mobio.pageObjects.CommonPageObject;
 		
 		//tags = "@chatdata,@chatcounter, @chatfilter, @chatsocket1, @chatsocket2, @chatother, @chatsocket3 ")
 		//tags = "@loginsuccess ,@chat3")		
-		tags = "@chatsocket3")		
+		tags = "@chatsocket1")		
 
 public class FunctionsRunner {
 	 
