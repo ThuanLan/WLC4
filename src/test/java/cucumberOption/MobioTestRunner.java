0f700d57cf1ea0b530eacc660abc92af688e27b5
ThuanLan <thuanlanmd@gmail.com>
2023-12-18 16:58:07 +0700

    Update Test run

--- src/test/java/cucumberOption/MobioTestRunner.java
+++ src/test/java/cucumberOption/MobioTestRunner.java
@@ -21,8 +21,8 @@ import mobio.pageObjects.CommonPageObject;
 						"com.cucumber.listener.ExtentCucumberFormatter:target/html/MobioExtentReport.html",
 						"json:target/site/mobio.json" },
 		snippets = SnippetType.CAMELCASE,
-		//tags = "@chatdata,@chatcounter, @chatfilter, @chatsocket1, @chatsocket2, @chatother, @chatsocket3 ")
-		tags = " @chatsocket1")
+		tags = "@chatdata,@chatcounter, @chatfilter, @chatsocket1, @chatsocket2, @chatother, @chatsocket3 ")
+		//tags = " @chatsocket1")
 		
 public class MobioTestRunner {
 	
