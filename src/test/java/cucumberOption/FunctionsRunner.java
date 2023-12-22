7aa6a4d69feb417ce0e816698d3c344b0007dd5c
ThuanLan <thuanlanmd@gmail.com>
2023-12-22 15:34:15 +0700

    cập nhật

--- src/test/java/cucumberOption/FunctionsRunner.java
+++ src/test/java/cucumberOption/FunctionsRunner.java
@@ -25,7 +25,7 @@ import mobio.pageObjects.CommonPageObject;
 		snippets = SnippetType.CAMELCASE,
 		
 		//tags = "@chatdata,@chatcounter, @chatfilter, @chatsocket1, @chatsocket2, @chatother, @chatsocket3 ")
-		//tags = "@loginsuccess ,@59")		
+		//tags = "@loginsuccess ")		
 		tags = "@loginsuccess,@chat11 ")		
 
 public class FunctionsRunner {
